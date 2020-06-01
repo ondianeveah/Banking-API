43ebb3d3c3b1f8af9c9317d987e50d7b862f36f5
efoxblack <erikmichaelfox@gmail.com>
2020-06-01 17:52:25 -0400

    Add getters and setters for the POJOS I completed

--- src/main/java/com/haggardinnovations/bankingapi/domain/Account.java
+++ src/main/java/com/haggardinnovations/bankingapi/domain/Account.java
@@ -37,4 +37,43 @@ public class Account {
     private Double balance;
     private Customer customer;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(Integer rewards) {
+        this.rewards = rewards;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }

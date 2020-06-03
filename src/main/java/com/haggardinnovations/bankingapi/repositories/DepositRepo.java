eb996154b53b3b86e11eef7b170a5c0de91a6e72
Andrew Oluoch <oluoch3@gmail.com>
2020-06-03 11:46:14 -0400

    added deposit service, edited deposit repo

--- src/main/java/com/haggardinnovations/bankingapi/repositories/DepositRepo.java
+++ src/main/java/com/haggardinnovations/bankingapi/repositories/DepositRepo.java
@@ -4,5 +4,9 @@ package com.haggardinnovations.bankingapi.repositories;
 import com.haggardinnovations.bankingapi.domains.Deposit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface DepositRepo extends CrudRepository<Deposit, Long> {
+    List<Deposit> findByAccountId(Long accountId);
 }
---
+++ src/main/java/com/haggardinnovations/bankingapi/services/DepositService.java
@@ -0,0 +1,42 @@
+package com.haggardinnovations.bankingapi.services;
+
+import com.haggardinnovations.bankingapi.domains.Deposit;
+import com.haggardinnovations.bankingapi.repositories.DepositRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepositService {
+
+    @Autowired
+    private DepositRepo depositRepo;
+
+    public List<Deposit> getAllDepositsByAccount(Long accountId){
+        List<Deposit> depositList = new ArrayList<>();
+        depositRepo.findByAccountId(accountId).forEach(depositList::add);
+        return depositList;
+    }
+    public Optional<Deposit> getOneDepositById(Long accountId){
+        return depositRepo.findById(accountId);
+    }
+    public void createDeposit(Deposit deposit, Long accountId){
+        depositRepo.save(deposit);
+    }
+    public void updateDeposit(Deposit deposit, Long depositId){
+        depositRepo.save(deposit);
+    }
+    public void deleteDeposit(Long id){
+        depositRepo.deleteById(id);
+    }
+
+
+
+
+
+
+}

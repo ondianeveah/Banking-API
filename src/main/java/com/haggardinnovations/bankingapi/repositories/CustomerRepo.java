bdc9c8d383c3f7333d6bbb7540903736f6034194
Shemar Rida <shemarrida1227@gmail.com>
2020-06-04 14:00:19 -0400

    fix what is commented out

--- src/main/java/com/haggardinnovations/bankingapi/repositories/CustomerRepo.java
+++ src/main/java/com/haggardinnovations/bankingapi/repositories/CustomerRepo.java
@@ -1,7 +1,7 @@
-package com.haggardinnovations.bankingapi.repositories;
-
-import com.haggardinnovations.bankingapi.domains.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepo extends CrudRepository<Customer, Long> {
-}
+//package com.haggardinnovations.bankingapi.repositories;
+//
+//import com.haggardinnovations.bankingapi.domains.Customer;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface CustomerRepo extends CrudRepository<Customer, Long> {
+//}

d6442a08fba2e4d4c22a1e0699bf6f0875a870f9
efoxblack <erikmichaelfox@gmail.com>
2020-06-17 11:04:41 -0400

    Update Account, Bill, and Customer service

--- src/main/java/com/haggardinnovations/bankingapi/services/CustomerService.java
+++ src/main/java/com/haggardinnovations/bankingapi/services/CustomerService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -57,7 +56,7 @@ public class CustomerService {
         Customer customer;
         for (Account account : accountRepo.findByCustomerId(accountId)) {
             if (account.getId().equals(accountId)) {
-                customer = account.getCustomerId();
+                customer = account.getCustomer();
                 return customer;
             }
         }
